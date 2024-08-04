efef1ec52a5b483d5190da7d1ea87604518c7ca8
mohid1214 <sumyalmuhammad@gmail.com>
2024-08-04 14:30:36 +0500

    reg+login+s3bucket

--- Server.js
+++ Server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 const userRoutes = require('./UserRoute');
 const userPicture = require('./UserImages')
 const { default: mongoose } = require("mongoose");
@@ -17,6 +16,8 @@ mongoose.connect('mongodb://localhost:27017/mydatabase')
 app.post('/users/createUsers',userRoutes.createUser);
 app.get('/users/login',userRoutes.ValidateLogin);
 app.get('/users/profilepicture',userPicture.giveProfilePicture);
+app.post('/users/geturlforprofileuploaad',userPicture.SendUploadUrlFE);
+app.put('/users/updateprofilepictureink',userPicture.updateProfilePicturelink)
 
 app.listen(3000, () => {
   console.log("server started om port 3000");
@@ -25,43 +26,3 @@ app.listen(3000, () => {
 
 
 
-// const s3client = new S3Client({
-//   region: "eu-north-1",
-//   credentials: {
-//     accessKeyId: "AKIAQEFWAZSSZDMJMH5O",
-//     secretAccessKey: "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",
-//   },
-// });
-
-// const {
-//   S3Client,
-//   GetObjectCommand,
-//   PutObjectCommand,
-// } = require("@aws-sdk/client-s3");
-
-// async function getobjectURL(key) {
-//   const command = new GetObjectCommand({
-//     Bucket: "sumyalsnewbucket",
-//     Key: key,
-//   });
-//   const url = await getSignedUrl(s3client, command);
-//   return url;
-// }
-
-// async function putObject(filename, filetype) {
-//   const command = new PutObjectCommand({
-//     Bucket: "sumyalsnewbucket",
-//     Key: `uploads/user-uploads/${filename}`,
-//     ContentType: filetype,
-//   });
-
-//   const url = await getSignedUrl(s3client, command);
-//   return url;
-// }
-
-// app.get("/geturl", async (req, res) => {
-//   const { filename, filetype } = req.query;
-//   const url = await putObject(filename, filetype);
-//   const s3Url = await getobjectURL(`uploads/user-uploads/${filename}`);
-//   res.send({ url, s3Url });
-// });
--- UserImages.js
+++ UserImages.js
@@ -1,12 +1,40 @@
-const { s3client, GetObjectCommand, PutObjectCommand } = require('@aws-sdk/client-s3')
+const { S3Client, GetObjectCommand, PutObjectCommand } = require('@aws-sdk/client-s3')
 const { getSignedUrl } = require('@aws-sdk/s3-request-presigner')
 const UserModel = require('./userdetails')
 
 
 
+const newS3Client = new S3Client({
+    region: "eu-north-1",
+    credentials: {
+        accessKeyId: 'AKIAQEFWAZSS3VUGMPLP',
+        secretAccessKey: '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'
+    }
+})
+
+const SendUploadUrlFE = async (req, res) => {
+    console.log("called")
+    console.log(req.body)
+    const { fileName, fileType } = req.body;
+    const result = await getUrlForUpload(fileName, fileType)
+    console.log(result)
+    res.send(result);
+
+}
+
+const getUrlForUpload = async (fileName, fileType) => {
+    const command = new PutObjectCommand({
+        Bucket: 'sumyalsnewbucket',
+        Key: `uploads/user-uploads/${fileName}`,
+        ContentType: fileType
+    })
+
+    const url = await getSignedUrl(newS3Client, command);
+    return url
+}
+
 
 const giveProfilePicture = async (req, res) => {
-    console.log("called");
     const { email } = req.query;
     console.log(email)
     const oldUser = await UserModel.findOne({ email });
@@ -15,10 +43,32 @@ const giveProfilePicture = async (req, res) => {
     }
 
     res.send(oldUser.profilePicture)
-    console.log(oldUser.profilePicture)
-    console.log("sending proile pic url")
 }
 
 
-module.exports = { giveProfilePicture }
+const getUrlForDownload = async (Key) =>{
+    const command = new GetObjectCommand({
+        Bucket:'sumyalsnewbucket',
+        Key: `uploads/user-uploads/${Key}`,
+    })
+    const url = await getSignedUrl(newS3Client,command)
+    return url;
+}
+const updateProfilePicturelink = async (req, res) => {
+    console.log("called")
+    const {email,name} = req.body;
+    console.log(email,name);
+    const downloadUrl = await getUrlForDownload(name)
+    console.log(downloadUrl)
+    const oldUser = await UserModel.findOne({email})
+    if(oldUser && downloadUrl){
+        console.log("download url")
+        oldUser.profilePicture = downloadUrl;
+        await oldUser.save();
+        res.send(downloadUrl)
+    }
+}
+
+
+module.exports = { giveProfilePicture, SendUploadUrlFE,updateProfilePicturelink }
 
